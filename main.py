c49aea57cc3ad1244be719bde56a2068f74090c9
Jung Gyu Yoon <yjg30737@gmail.com>
2023-10-31 17:47:20 +0900

    add seaborn library to add heatmap, set the title, update the gui

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ project_root = os.path.dirname(os.path.dirname(script_path))
 sys.path.insert(0, project_root)
 sys.path.insert(0, os.getcwd())  # Add the current directory as well
 
-from PyQt5.QtWidgets import QMainWindow, QComboBox, QVBoxLayout, QWidget, QApplication
+from PyQt5.QtWidgets import QMainWindow, QComboBox, QVBoxLayout, QWidget, QApplication, QScrollArea
 from PyQt5.QtCore import Qt, QCoreApplication
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 
@@ -38,14 +39,26 @@ class MainWindow(QMainWindow):
         columnCmbBox.addItems(self.__df.columns)
         columnCmbBox.currentIndexChanged.connect(self.updatePlot)
 
-        self.__figure = plt.figure(figsize=(8, 8))
-        self.__axis = self.__figure.add_subplot(111)
+        self.__figure1 = plt.figure(figsize=(8, 8))
+        self.__figure2 = plt.figure(figsize=(8, 8))
 
-        canvas = FigureCanvas(self.__figure)
+        self.__axis1 = self.__figure1.add_subplot(111)
+
+        canvas1 = FigureCanvas(self.__figure1)
+        canvas2 = FigureCanvas(self.__figure2)
+
+        lay = QVBoxLayout()
+        lay.addWidget(canvas1)
+        lay.addWidget(canvas2)
+        graphScrollWidget = QWidget()
+        graphScrollWidget.setLayout(lay)
+
+        graphScrollArea = QScrollArea()
+        graphScrollArea.setWidget(graphScrollWidget)
 
         lay = QVBoxLayout()
         lay.addWidget(columnCmbBox)
-        lay.addWidget(canvas)
+        lay.addWidget(graphScrollArea)
 
         mainWidget = QWidget()
         mainWidget.setLayout(lay)
@@ -53,20 +66,30 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(mainWidget)
 
         self.__setHist(self.__df.columns[0])
+        self.__setSeaborn()
 
     def updatePlot(self):
         selected_column = self.sender().currentText()
         self.__setHist(selected_column)
 
+    def __setSeaborn(self):
+        colormap = plt.cm.gist_heat
+        plt.title('Heatmap')
+        sns.heatmap(self.__df.corr(), linewidths=0.1, vmax=0.5, cmap=colormap, linecolor='white', annot=True)
+        self.__figure2.canvas.draw()
+        
     def __setHist(self, column_name):
-        self.__axis.clear()
+        self.__axis1.clear()
 
-        self.__axis.hist(
+        context = f'Correlation between diabetes and {column_name}'
+
+        self.__axis1.set_title(context)
+        self.__axis1.hist(
             x=[self.__df[self.__df.diabetes == 0][column_name], self.__df[self.__df.diabetes == 1][column_name]],
             bins=30, histtype='barstacked', label=['normal', 'diabetes'])
 
-        self.__axis.legend()
-        self.__figure.canvas.draw()
+        self.__axis1.legend()
+        self.__figure1.canvas.draw()
 
 
 if __name__ == '__main__':
